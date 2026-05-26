6085a4a414f5196f390f2ddf733915d8be42d1f8
Kaiquii <112667885+Kaiquii@users.noreply.github.com>
2026-05-26 10:10:12 -0300

    Liberei o Cors para qlq IP

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Ticket Backend API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
